f0e570be6675c876d0f2855062ed8682176b7f5a
Kyle Drake <kyle@kyledrake.net>
2015-09-03 01:21:38 -0700

    initial checkin

---
+++ app.js
@@ -0,0 +1,31 @@
+var httpProxy = require('http-proxy')
+var http      = require('http')
+var base32    = require('base32.js')
+var base58    = require('base58-native')
+var proxy     = httpProxy.createProxyServer()
+
+http.createServer(function(req, res) {
+  var hshcaRegexFailure = false
+
+  try {
+    var hshca = req.headers.host.match(/^(.+)\.ipfs\.neocitiesops\.net$/i)[1]
+  } catch(e) {
+    console.log(e)
+    hshcaRegexFailure = true
+  }
+
+  if(hshcaRegexFailure) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Invalid HSHCA hash for archive lookup\n')
+  } else {
+    var decoder = new base32.Decoder({ type: "rfc4648" })
+    var multihash = decoder.write(hshca.toUpperCase()).finalize()
+    var ipfsHash = base58.encode(multihash)
+
+    console.log(ipfsHash)
+
+    proxy.web(req, res, { target: 'http://ipfs.neocitiesops.net:8080/ipfs/'+ipfsHash })
+  }
+}).listen(80, '0.0.0.0')
+
+console.log('Server running')
