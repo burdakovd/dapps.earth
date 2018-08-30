652a259ae1b26b6be5507ff400540fd62c88a05e
Daniil Burdakov <icqkill@gmail.com>
2018-08-30 12:26:31 +0000

    build main page on server, and no JS on client

--- html/webpack.config.js
+++ html/webpack.config.js
@@ -1,45 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const variablesBuilder = require('./src/variables.js');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        use: [
-            {
-                loader: "html-loader"
-            },
-            {
-                loader: "markdown-loader",
-                options: {}
-            }
-          ]
-      },
-      {
-          test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inlineSource: '.(js|css)$'
-    }),
-    new HtmlWebpackInlineSourcePlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: 'static/*.*',
-        to: './',
-        flatten: true,
-      }
-    ], {})
-  ]
-}
+module.exports = () => variablesBuilder().then(
+  variables => ({
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inlineSource: '.(js|css)$',
+        README_CSS: variables.README_CSS,
+        README_HTML_CONTENT: variables.README,
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+      new CopyWebpackPlugin([
+        {
+          from: 'static/*.*',
+          to: './',
+          flatten: true,
+        }
+      ], {})
+    ],
+  })
+);
