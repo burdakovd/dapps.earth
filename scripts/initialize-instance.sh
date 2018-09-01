8017ce424f2b3c7f5d8d5602045337036c392c10
Daniil Burdakov <icqkill@gmail.com>
2018-09-01 16:56:45 +0000

    use docker compose 1.22.0

--- scripts/initialize-instance.sh
+++ scripts/initialize-instance.sh
@@ -75,7 +75,7 @@ systemctl enable awslogsd.service
 yum update -y
 yum install -y docker htop
 usermod -a -G docker ec2-user
-curl -f -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` \
+curl -f -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` \
   -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 chkconfig docker on
