f3fcd48071cd1421f95aca4671208bb9e7899377
Daniil Burdakov <icqkill@gmail.com>
2018-09-03 22:01:25 +0000

    support more subdomains, bzz, bzz-hash, bzz-immutable, redirects

--- proxy/app.js
+++ proxy/app.js
@@ -1,5 +1,6 @@
 var httpProxy = require('http-proxy')
 var http      = require('http')
+var base32    = require('base32.js')
 var CID       = require('cids')
 var process   = require('process')
 var url       = require('url')
@@ -61,13 +62,68 @@ var handlers = {
       },
     )
   },
+  // if someone is accessing swarm without subdomain, redirect them
+  ['^(bzz(-[a-z]+)?)\\.' + regexify(BASE_DOMAIN) + '$']: function(req, res, match) {
+    var path = url.parse(req.url).pathname
+    if (path === '/') {
+      res.writeHead(
+        302,
+        {'Location': 'https://dapps.earth/'},
+      );
+      res.end('')
+      return;
+    }
+
+    var matches = path.match(
+      '^/(bzz(?:-[a-z]+)?):/([a-z0-9-.]+)((?:/.*)?)$',
+    );
+
+    if (matches == null) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Unrecognized path: ' + path + '\n')
+      return;
+    }
+
+    var swarm_protocol = matches[1];
+    var name = matches[2];
+    var path = matches[3];
+
+    const encodedName = (() => {
+      if (swarm_protocol !== 'bzz-immutable') {
+        return name;
+      }
+      const bytes = Buffer.from(name, 'hex');
+      var encoder = new base32.Encoder({ type: "rfc4648" });
+      var hash = encoder.write(bytes).finalize();
+      return hash;
+    })();
+
+    var newDestination = (HAS_SSL ? 'https://' : 'http://') +
+      encodedName + '.' + swarm_protocol + '.' + BASE_DOMAIN + path;
+    console.log(req.url, '=>', newDestination);
+    res.writeHead(302, {'Location': newDestination});
+    res.end('')
+  },
   // if someone is accessing swarm subdomain, proxy to swarm
-  ['^(.+)\\.bzz\\.' + regexify(BASE_DOMAIN) + '$']: function(req, res, match) {
+  ['^(.+)\\.(bzz(-[a-z]+)?)\\.' + regexify(BASE_DOMAIN) + '$']: function(req, res, match) {
     var name = match[1];
+    var swarm_protocol = match[2];
+    const decodedName = (() => {
+      if (swarm_protocol !== 'bzz-immutable') {
+        return name;
+      }
+      var decoder = new base32.Decoder({ type: "rfc4648" });
+      var hash = decoder.write(name.toUpperCase()).finalize();
+      console.log(hash);
+      return [...hash].map(
+        byte => ('0' + (byte & 0xFF).toString(16)).slice(-2),
+      ).join('');
+    })();
     var base = RUN_LOCAL_SWARM === '1'
       ? 'http://swarm:8500'
       : 'https://swarm-gateways.net';
-    var target = base + '/bzz:/' + name;
+    var target = base + '/' + swarm_protocol + ':/' + decodedName;
+    console.log(req.url, '=>', target);
     proxy.web(
       req,
       res,
